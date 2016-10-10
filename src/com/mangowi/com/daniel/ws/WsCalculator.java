96f06edb5eee0a135c3b3bbbef10ba35ba2bd25c
Daniel Mangowi <danielmangowi@gmail.com>
2016-10-10 15:21:34 +0200

    Initial commit

---
+++ src/com/mangowi/com/daniel/ws/WsCalculator.java
@@ -0,0 +1,10 @@
+package com.mangowi.com.daniel.ws;
+
+public interface WsCalculator {
+  public Integer avg(int a, int b, int c);
+  public Integer max(int a, int c);
+}
+
+/*
+*java2wsdl -cn com.mangowi.daniel.ws.ArgWs
+*/
